8d892a54665f7fc4aa693cdc30d15417f831b80a
syed shouzeb hassan <38203938+shouzeb@users.noreply.github.com>
2020-02-25 23:33:21 +0500

    Add files via upload

---
+++ iris dataset.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+s
+"""
+
+Created on Tue Feb 25 19:57:59 2020
+
+@author: Shouzeb
+
+"""
+
+from sklearn.datasets import load_iris
+iris=load_iris()
+print(iris.feature_names)
